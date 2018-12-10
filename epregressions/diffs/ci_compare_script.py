b27025a8160432f170b4a8f9f2b52ecaa58a1e10
Edwin Lee <leeed2001@gmail.com>
2018-12-10 12:44:59 -0600

    Add CI regression script and unit tests

---
+++ epregressions/diffs/ci_compare_script.py
@@ -0,0 +1,308 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import glob
+import json
+import os
+import sys
+from datetime import datetime
+
+import boto
+
+from epregressions.builds.install import EPlusInstallDirectory
+from epregressions.runtests import SuiteRunner, TestRunConfiguration, TestEntry
+from epregressions.structures import ForceRunType, ReportingFreq
+from epregressions.structures import TextDifferences
+
+
+def get_diff_files(out_dir):
+    files = glob.glob(os.path.join(out_dir, "*.*.*"))
+    return files
+
+
+def cleanup(out_dir):
+    for this_file in get_diff_files(out_dir):
+        os.remove(this_file)
+
+
+def print_message(msg):
+    print("[decent_ci:test_result:message] " + msg)
+
+
+def process_diffs(diff_name, diffs, this_has_diffs, this_has_small_diffs):
+    if not diffs:
+        return this_has_diffs, this_has_small_diffs
+    if diffs.diff_type == 'Big Diffs':
+        this_has_diffs = True
+        print_message(diff_name + " big diffs.")
+    elif diffs.diff_type == 'Small Diffs':
+        this_has_small_diffs = True
+        print_message(diff_name + " small diffs.")
+    return this_has_diffs, this_has_small_diffs
+
+
+def main_function(file_name, base_dir, mod_dir, base_sha, mod_sha, make_public, device_id, test_mode):
+
+    print("Device id: %s" % device_id)
+
+    # build type really doesn't matter, so use the simplest one, the E+ install
+    base = EPlusInstallDirectory()
+    base.set_build_directory(base_dir)
+    base.run = False
+    mod = EPlusInstallDirectory()
+    mod.set_build_directory(mod_dir)
+    mod.run = False
+
+    run_config = TestRunConfiguration(
+        force_run_type=ForceRunType.NONE,
+        single_test_run=True,
+        num_threads=1,
+        report_freq=ReportingFreq.HOURLY,
+        build_a=base,
+        build_b=mod
+    )
+
+    print("Comparing `{0}` with `{1}`".format(base_dir, mod_dir))
+
+    initial_entry = TestEntry(file_name, "")
+    runner = SuiteRunner(run_config, [])
+
+    cleanup(mod_dir)
+    entry = runner.process_diffs_for_one_case(initial_entry, ci_mode=True)  # returns an updated entry
+
+    with open('results.json', 'w') as f:
+        f.write(json.dumps(entry.to_dict(), indent=4))
+
+    success = True
+    has_diffs = False
+    has_small_diffs = False
+
+    # Note, comment out any of the "has_diffs" below if you don't want
+    # it to generate an error condition
+
+    if entry.aud_diffs and (entry.aud_diffs.diff_type != TextDifferences.EQUAL):
+        has_small_diffs = True
+        print_message("AUD diffs.")
+
+    if entry.bnd_diffs and (entry.bnd_diffs.diff_type != TextDifferences.EQUAL):
+        has_small_diffs = True
+        print_message("BND diffs.")
+
+    if entry.dl_in_diffs and (entry.dl_in_diffs.diff_type != TextDifferences.EQUAL):
+        has_small_diffs = True
+        print_message("delightin diffs.")
+
+    if entry.dl_out_diffs and (entry.dl_out_diffs.diff_type != TextDifferences.EQUAL):
+        has_small_diffs = True
+        print_message("delightout diffs.")
+
+    if entry.dxf_diffs and (entry.dxf_diffs.diff_type != TextDifferences.EQUAL):
+        has_small_diffs = True
+        print_message("DXF diffs.")
+
+    if entry.eio_diffs and (entry.eio_diffs.diff_type != TextDifferences.EQUAL):
+        has_small_diffs = True
+        print_message("EIO diffs.")
+
+    if entry.err_diffs and (entry.err_diffs.diff_type != TextDifferences.EQUAL):
+        has_small_diffs = True
+        print_message("ERR diffs.")
+
+    # numeric diff
+    if entry.eso_diffs:
+        has_diffs, has_small_diffs = process_diffs("ESO", entry.eso_diffs, has_diffs, has_small_diffs)
+
+    if entry.mdd_diffs and (entry.mdd_diffs.diff_type != TextDifferences.EQUAL):
+        has_small_diffs = True
+        print_message("MDD diffs.")
+
+    if entry.mtd_diffs and (entry.mtd_diffs.diff_type != TextDifferences.EQUAL):
+        has_small_diffs = True
+        print_message("MTD diffs.")
+
+    # numeric diff
+    if entry.mtr_diffs:
+        has_diffs, has_small_diffs = process_diffs("MTR", entry.mtr_diffs, has_diffs, has_small_diffs)
+
+    if entry.rdd_diffs and (entry.rdd_diffs.diff_type != TextDifferences.EQUAL):
+        has_small_diffs = True
+        print_message("RDD diffs.")
+
+    if entry.shd_diffs and (entry.shd_diffs.diff_type != TextDifferences.EQUAL):
+        has_small_diffs = True
+        print_message("SHD diffs.")
+
+    # numeric diff
+    if entry.ssz_diffs:
+        has_diffs, has_small_diffs = process_diffs("SSZ", entry.ssz_diffs, has_diffs, has_small_diffs)
+
+    # numeric diff
+    if entry.zsz_diffs:
+        has_diffs, has_small_diffs = process_diffs("ZSZ", entry.zsz_diffs, has_diffs, has_small_diffs)
+
+    if entry.table_diffs:
+        if entry.table_diffs.big_diff_count > 0:
+            has_diffs = True
+            print_message("Table big diffs.")
+        elif entry.table_diffs.small_diff_count > 0:
+            has_small_diffs = True
+            print_message("Table small diffs.")
+
+    if has_small_diffs:
+        print("[decent_ci:test_result:warn]")
+
+    if test_mode:
+        print("Skipping Amazon upload in test_mode operation")
+    elif has_small_diffs or has_diffs:
+        conn = boto.connect_s3()
+        bucket_name = 'energyplus'
+        bucket = conn.get_bucket(bucket_name)
+
+        potential_files = get_diff_files(mod_dir)
+
+        date = datetime.now()
+        date_str = "%d-%02d" % (date.year, date.month)
+        file_dir = "regressions/{0}/{1}-{2}/{3}/{4}".format(date_str, base_sha, mod_sha, file_name, device_id)
+
+        found_files = []
+        for filename in potential_files:
+            file_path_to_send = filename
+
+            # print("Processing output file: {0}".format(filepath_to_send))
+            if not os.path.isfile(file_path_to_send):
+                continue
+            if not os.stat(file_path_to_send).st_size > 0:
+                print("File is empty, not sending: {0}".format(file_path_to_send))
+                continue
+
+            try:
+                file_path = "{0}/{1}".format(file_dir, os.path.basename(filename))
+                # print("Processing output file: {0}, uploading to: {1}".format(filepath_to_send, filepath))
+
+                key = boto.s3.key.Key(bucket, file_path)
+                file_to_send = open(file_path_to_send, 'r')
+                key.set_contents_from_string(file_to_send.read())
+
+                if make_public:
+                    key.make_public()
+
+                htmlkey = boto.s3.key.Key(bucket, file_path + ".html")
+                htmlkey.set_contents_from_string("""
+                        <!doctype html>
+                        <html>
+                          <head>
+                            <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+                            <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+                            <link href="//alexgorbatchev.com/pub/sh/current/styles/shCore.css" rel="stylesheet" type="text/css" />
+                            <link href="//alexgorbatchev.com/pub/sh/current/styles/shThemeDefault.css" rel="stylesheet" type="text/css" />
+                            <script src="//alexgorbatchev.com/pub/sh/current/scripts/shCore.js" type="text/javascript"></script>
+                            <script src="//alexgorbatchev.com/pub/sh/current/scripts/shAutoloader.js" type="text/javascript"></script>
+                            <script src="//alexgorbatchev.com/pub/sh/current/scripts/shBrushPhp.js" type="text/javascript"></script>
+                            <script src="//alexgorbatchev.com/pub/sh/current/scripts/shBrushPlain.js" type="text/javascript"></script>
+                            <script src="//alexgorbatchev.com/pub/sh/current/scripts/shBrushDiff.js" type="text/javascript"></script>
+                          </head>
+                          <body>
+
+                            <script>
+                              filename = '/""" + file_path + """'
+                            </script>
+
+                            <div id="codeholder">
+                            </div>
+
+                            <script>
+                              $.get(filename , function( data ) {
+                                elem = document.createElement("pre");
+                                brush = "plain";
+                                if (filename.indexOf(".dif") != -1)
+                                {
+                                  brush = "diff";
+                                } 
+                                elem.setAttribute("class", "brush: " + brush + "; gutter: true;");
+                                elem.appendChild(document.createTextNode(data.replace("<", "&lt;")));
+                                document.getElementById("codeholder").appendChild(elem);
+                                SyntaxHighlighter.highlight(elem);
+                              });
+
+                              SyntaxHighlighter.all();
+                            </script>
+                          </body>
+                        </html>""", headers={"Content-Type": "text/html"})
+
+                if make_public:
+                    htmlkey.make_public()
+
+                found_files.append(filename)
+            except Exception as e:
+                success = False
+                print("There was a problem processing file: %s" % e)
+
+        if len(found_files) > 0:
+            try:
+                htmlkey = boto.s3.key.Key(bucket, file_dir + "/index.html")
+                index = """
+                        <!doctype html>
+                        <html>
+                          <head>
+                            <!-- Latest compiled and minified CSS -->
+                            <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+                            <!-- Optional theme -->
+                            <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+
+                            <!-- Latest compiled and minified JavaScript -->
+                            <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+                            <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+                            <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+                          </head>
+                          <body>
+
+                            <table class='table table-hover'>
+                              <tr><th>filename</th><th></th><th></th></tr>"""
+
+                for filename in found_files:
+                    filepath = "{0}/{1}".format(file_dir, os.path.basename(filename))
+                    index += "<tr><td>" + os.path.basename(
+                        filename) + "</td><td><a href='/" + filepath + "'>download</a></td><td><a href='/" + filepath + ".html'>view</a></td></tr>"
+
+                index += """
+                            </table>
+                          </body>
+                        </html>"""
+
+                htmlkey.set_contents_from_string(index, headers={"Content-Type": "text/html"})
+
+                if make_public:
+                    htmlkey.make_public()
+
+                url = "http://{0}.s3-website-{1}.amazonaws.com/{2}".format(bucket_name, "us-east-1", file_dir)
+                print("<a href='{0}'>Regression Results</a>".format(url))
+            except Exception as e:
+                success = False
+                print("There was a problem generating results webpage: %s" % e)
+
+    if success and not has_diffs:
+        print("Success")
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 8:
+        print("syntax: %s file_name base_dir mod_dir base_sha mod_sha make_public device_id [test]" % sys.argv[0])
+        sys.exit(1)
+
+    arg_file_name = sys.argv[1]
+    arg_base_dir = sys.argv[2]
+    arg_mod_dir = sys.argv[3]
+    arg_base_sha = sys.argv[4]
+    arg_mod_sha = sys.argv[5]
+    arg_make_public = sys.argv[6].lower() == "true".lower()
+    arg_device_id = sys.argv[7]
+    _test_mode = False
+    if len(sys.argv) > 8 and sys.argv[8].upper() == 'TEST':
+        _test_mode = True
+    main_function(
+        arg_file_name, arg_base_dir, arg_mod_dir, arg_base_sha, arg_mod_sha, arg_make_public, arg_device_id, _test_mode
+    )
---
+++ epregressions/tests/diffs/test_ci_compare_script.py
@@ -0,0 +1,144 @@
+import glob
+import os
+import shutil
+import sys
+import tempfile
+import unittest
+from contextlib import contextmanager
+from io import StringIO
+
+from epregressions.diffs.ci_compare_script import cleanup, get_diff_files, main_function, print_message, process_diffs
+from epregressions.runtests import TestEntry
+from epregressions.structures import MathDifferences
+
+
+@contextmanager
+def captured_output():
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
+
+
+class TestCICompareScriptFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.cur_dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.csv_resource_dir = os.path.join(self.cur_dir_path, 'csv_resources')
+        self.tbl_resource_dir = os.path.join(self.cur_dir_path, 'tbl_resources')
+        self.temp_dir = tempfile.mkdtemp()
+        self.temp_base_dir = tempfile.mkdtemp()
+        self.temp_mod_dir = tempfile.mkdtemp()
+
+    def test_get_diff_files(self):
+        file_names = [
+            'eplusout.rdd',
+            'eplusout.rdd.diffs',
+            'eplusout.csv',
+            'eplusout.csv.absdiffs.csv'
+        ]
+        for file_name in file_names:
+            full_path = os.path.join(self.temp_dir, file_name)
+            with open(full_path, 'w'):
+                pass  # just let it close the file
+        self.assertEqual(2, len(get_diff_files(self.temp_dir)))
+
+    def test_cleanup(self):
+        file_names = [
+            'eplusout.rdd',
+            'eplusout.rdd.diffs',
+            'eplusout.csv',
+            'eplusout.csv.absdiffs.csv'
+        ]
+        for file_name in file_names:
+            full_path = os.path.join(self.temp_dir, file_name)
+            with open(full_path, 'w'):
+                pass  # just let it close the file
+        files_found = glob.glob(os.path.join(self.temp_dir, "*"))
+        self.assertEqual(4, len(files_found))
+        cleanup(self.temp_dir)
+        files_found = glob.glob(os.path.join(self.temp_dir, "*"))
+        self.assertEqual(2, len(files_found))
+
+    def test_print_message(self):
+        with captured_output() as (out, err):
+            print_message("Hello world!")
+        # This can go inside or outside the `with` block
+        output = out.getvalue().strip()
+        self.assertIn('decent_ci:test_result:message', output)
+        self.assertIn('Hello', output)
+
+    def test_process_diffs(self):
+        # process diffs should never turn a True to a False!
+        entry = TestEntry('MyFileName', 'MyWeather.epw')
+        # try a blank entry
+        initial_has_diffs = False
+        initial_has_small_diffs = False
+        new_diffs, new_small_diffs = process_diffs("ESO", entry.eso_diffs, initial_has_diffs, initial_has_small_diffs)
+        self.assertFalse(new_diffs)
+        self.assertFalse(new_small_diffs)
+        # now add some only small diff to it
+        entry.add_math_differences(MathDifferences(['Small Diffs', 2, 0, 4]), MathDifferences.ESO)
+        with captured_output() as (out, err):
+            new_diffs, new_small_diffs = process_diffs("ESO", entry.eso_diffs, new_diffs, new_small_diffs)
+            self.assertFalse(new_diffs)
+            self.assertTrue(new_small_diffs)
+            self.assertIn('ESO small diffs', out.getvalue().strip())
+        # now add some only big diff to it
+        new_small_diffs = False
+        new_diffs = False
+        entry.add_math_differences(MathDifferences(['Big Diffs', 2, 3, 0]), MathDifferences.ESO)
+        with captured_output() as (out, err):
+            new_diffs, new_small_diffs = process_diffs("ESO", entry.eso_diffs, new_diffs, new_small_diffs)
+            self.assertTrue(new_diffs)
+            self.assertFalse(new_small_diffs)
+            self.assertIn('ESO big diffs', out.getvalue().strip())
+        # now go back to a blank one, any True values should still be True!
+        entry.eso_diffs = None
+        new_diffs, new_small_diffs = process_diffs("ESO", entry.eso_diffs, new_diffs, new_small_diffs)
+        self.assertTrue(new_diffs)
+        self.assertFalse(new_small_diffs)
+
+    def test_main_function(self):
+        shutil.copy(
+            os.path.join(self.csv_resource_dir, 'eplusout.csv'),
+            os.path.join(self.temp_base_dir, 'eplusout.csv')
+        )
+        shutil.copy(
+            os.path.join(self.csv_resource_dir, 'eplusout_big_watt_diffs.csv'),
+            os.path.join(self.temp_mod_dir, 'eplusout.csv')
+        )
+        with captured_output() as (out, err):
+            # should fail if we don't have any .end files
+            main_function(
+                file_name='HVACTemplate-5ZoneFanCoil',
+                base_dir=self.temp_base_dir,
+                mod_dir=self.temp_mod_dir,
+                base_sha='base123',
+                mod_sha='mod456',
+                make_public=True,
+                device_id='some_device_id',
+                test_mode=True
+            )
+            self.assertIn('Skipping', out.getvalue().strip())
+        end_string = 'EnergyPlus Completed Successfully-- 0 Warning; 0 Severe Errors; Elapsed Time=00hr 00min  3.06sec'
+        with open(os.path.join(self.temp_base_dir, 'eplusout.end'), 'w') as f1:
+            f1.write(end_string)
+        with open(os.path.join(self.temp_mod_dir, 'eplusout.end'), 'w') as f2:
+            f2.write(end_string)
+        with captured_output() as (out, err):
+            # should fail if we don't have any .end files
+            main_function(
+                file_name='HVACTemplate-5ZoneFanCoil',
+                base_dir=self.temp_base_dir,
+                mod_dir=self.temp_mod_dir,
+                base_sha='base123',
+                mod_sha='mod456',
+                make_public=True,
+                device_id='some_device_id',
+                test_mode=True
+            )
+            self.assertIn('ESO big diffs', out.getvalue().strip())
