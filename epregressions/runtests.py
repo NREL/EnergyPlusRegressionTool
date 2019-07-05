6879824d8370082742ecf80b60345d54a4268110
Edwin Lee <leeed2001@gmail.com>
2019-07-04 23:17:32 -0500

    Go back to prior fix but change something

--- epregressions/runtests.py
+++ epregressions/runtests.py
@@ -525,9 +525,9 @@ class SuiteRunner:
                     diffs.append("Key error in GLHE object named \"%s\"; something doesn't match" % glhe_name)
         with io.open(diff_file, 'w', encoding='utf-8') as out_file:
             my_json_str = json.dumps({"diffs": diffs}, ensure_ascii=False)
-            if sys.version_info[0] == 2:
-                my_json_str = unicode(my_json_str, "utf-8")
-            out_file.write(json.dumps(my_json_str))
+            if isinstance(my_json_str, str):  # python 2
+                my_json_str = my_json_str.decode("utf-8")
+            out_file.write(my_json_str)
         return TextDifferences.DIFFS
 
     def process_diffs_for_one_case(self, this_entry, ci_mode=False):
