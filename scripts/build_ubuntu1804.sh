f87438bbffbb40328317610ae9864f4973baec55
Edwin Lee <leeed2001@gmail.com>
2020-10-18 15:51:15 -0500

    Fix mac and linux pyinstaller commands

--- scripts/build_ubuntu1804.sh
+++ scripts/build_ubuntu1804.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-pyinstaller --onefile --add-data "epregressions/diffs/math_diff.config;epregressions/diffs" eplus_regression_runner
+pyinstaller --onefile --add-data "epregressions/diffs/math_diff.config:epregressions/diffs" eplus_regression_runner
 mkdir deploy
 tar -zcvf deploy/EnergyPlusRegressionTool-Ubuntu1804.tar.gz -C dist main
