d54db6e54fbcfdea03a78b516c0cec1583bcf22b
Edwin Lee <leeed2001@gmail.com>
2020-10-20 15:49:13 -0500

    Take out malformed email address

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     url='https://github.com/NREL/EnergyPlusRegressionTool',
     license='',
     author='Edwin Lee',
-    author_email='leeed2001 at gmail.com',
+    author_email='',
     description='A Python 3 library for evaluating regressions between EnergyPlus builds.',
     long_description=long_description,
     long_description_content_type='text/markdown',
