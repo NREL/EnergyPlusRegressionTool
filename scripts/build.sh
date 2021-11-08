b5b188a18d47de2465d61a609d90a6a942cd0b49
Edwin Lee <leeed2001@gmail.com>
2021-11-08 12:23:59 -0600

    Eliminate build script usage

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,25 +4,20 @@ BUILD_CONFIG=$1
 
 VERSION_STRING=$(grep VERSION epregressions/__init__.py | cut -d= -f2 | xargs)
 
-rm -rf deploy
-
 case ${BUILD_CONFIG} in
 
   Ubuntu20)
     pyinstaller --onefile --add-data "epregressions/diffs/math_diff.config:epregressions/diffs" epregressions/runner.py
-    mkdir deploy
     tar -zcvf deploy/EnergyPlusRegressionTool-"${VERSION_STRING}"-Ubuntu20.04.tar.gz -C dist runner
     ;;
 
   Mac)
     pyinstaller --onefile --noconsole --add-data "epregressions/diffs/math_diff.config:epregressions/diffs" epregressions/runner.py
-    mkdir deploy
     tar -zcvf deploy/EnergyPlusRegressionTool-"${VERSION_STRING}"-Mac.tar.gz -C dist runner.app
     ;;
 
   Windows)
     pyinstaller --onefile --noconsole --add-data "epregressions/diffs/math_diff.config;epregressions/diffs" epregressions/runner.py
-    mkdir deploy
     7z.exe a deploy/EnergyPlusRegressionTool-"${VERSION_STRING}"-Windows.zip ./dist/*
     ;;
 
