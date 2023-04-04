464638c067e145c4ff1d05caacf1e0872fa3f695
Edwin Lee <leeed2001@gmail.com>
2023-04-04 14:11:46 -0500

    Use newer version of Plan tools

--- energyplus_regressions/configure.py
+++ energyplus_regressions/configure.py
@@ -2,7 +2,8 @@ from plan_tools.entry_point import EntryPoint
 
 
 def configure_cli() -> None:
+    source_dir = "energyplus_regressions"
     name = "energyplus_regression_runner"
     nice_name = "EnergyPlus Regression Tool"
-    s = EntryPoint(name, nice_name, "An EnergyPlus test suite utility", name)
+    s = EntryPoint(source_dir, name, nice_name, "An EnergyPlus test suite utility", name)
     s.run()
